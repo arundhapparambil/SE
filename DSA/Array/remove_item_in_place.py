45327436b6ca45b9d387abc40c1c2ae1715b47b2
Arun Haridas Dhapparambil <arundhapparambil@outlook.com>
2024-10-26 17:03:26 +1100

    remove item

---
+++ DSA/Array/remove_item_in_place.py
@@ -0,0 +1,11 @@
+nums = [0,0,1,1,1,2,2,3,3,3,4,5,6,7,7,7,8,9]
+print(nums)
+
+j = 0
+del_num = 3
+for i in  range(1,len(nums)):
+	if nums[i] != nums[j]:
+		j+=1
+		nums[j] = nums[i]
+print(nums,j)
+
